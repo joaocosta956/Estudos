14352ec98ca7b511f462454ff7724cb8a1f064af
joaocosta956 <joaocosta956@gmail.com>
2025-07-01 14:29:58 -0300

    Cheks

--- aula158/banco.py
+++ aula158/banco.py
@@ -16,7 +16,7 @@ class Banco:
         return f'{class_name},{attr}'
 
     def _check_conta(self,conta):
-        if conta.conta in self.conta:
+        if conta in self.conta:
             return True
         print('conta')
         return False
@@ -33,30 +33,37 @@ class Banco:
         print('cliente')
         return False
     
-    def autenticar(self, conta, cliente):
+    # Importante ter porque se um cliente estiver cadastrado e ele colocar uma conta de outro cliente,
+    # por exemplo, c1 não tem saldo, mas a c2 tem, poderia colocar a c1 com a conta da c2 e passaria pela
+    # autenticação
+    def check_acc(self, conta, cliente):
+        if conta is cliente.conta:
+            return True
+        return False
+    
+    def autenticar(self, conta : contas.Conta, cliente : pessoa.Cliente):
         return self._check_agencia(conta) and \
         self._check_cliente(cliente) and \
-        self._check_conta(conta)
+        self._check_conta(conta) and self.check_acc(conta, cliente)
     
 
 
 if __name__ == '__main__':
     c1 = pessoa.Cliente('Joao', 38)
     c1.conta = contas.ContaCorrente(112, 1300, 150, 100)
-    print(c1.nome)
-    print(c1.idade)
-    print(c1.conta)
+    # print(c1.nome)
+    # print(c1.idade)
     c2 = pessoa.Cliente('Maria', 18)
     c2.conta = contas.ContaPoupanca(111, 1500, 0)
-    print(c2.nome)
-    print(c2.idade)
-    print(c2.conta)
+    # print(c2.nome)
+    # print(c2.idade)
 
     banco = Banco()
     banco.cliente.extend([c1,c2])
-    banco.conta.extend([c1.conta.conta,c2.conta.conta])
+    banco.conta.extend([c1.conta,c2.conta])
     banco.agencia.extend([111,222])
     print(banco)
 
-    print(banco.autenticar(c1.conta, c1))
-    print(banco.autenticar(c2.conta, c2))
+    print(banco.autenticar(c1.conta, c2))
+    print(banco.autenticar(c2.conta, c1))
+    c1.conta.sacar(1)
